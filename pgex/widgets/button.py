28de1e72dfbaf025886bec37b733685cb345439e
IvanFoke <58694429+IvanFoke@users.noreply.github.com>
2020-07-06 16:04:17 +0300

    button widget

--- pgex/widgets/button.py
+++ pgex/widgets/button.py
@@ -1,12 +1,37 @@
 from pgex.widgets.text import Text
-from pygame import *
+from pygame import draw, mouse, mixer, time
 
 
 class Button:
-    def __init__(self, width, height, text, action=None, bg=(13, 162, 58), active_bg=(23, 204, 58)):
+    def __init__(self, width, height, text, font_path, font_size, sound_path=None, font_color=(0, 0, 0),
+                 inactive_bg=(13, 162, 58), active_bg=(20, 180, 58), pressed_bg=(32, 215, 58), action=lambda: None):
         self.width = width
         self.height = height
-        self.text = text
+        self.text = Text(text, font_path, font_size, font_color)
         self.action = action
-        self.bg = bg
+        self.inactive_bg = inactive_bg
         self.active_bg = active_bg
+        self.pressed_bg = pressed_bg
+        self.sound = mixer.Sound(sound_path) if sound_path else None
+
+    def draw(self, screen, coordinates):
+        x, y = coordinates
+        mouse_pos, mouse_click = mouse.get_pos(), mouse.get_pressed()
+        text_rect = self.text.get_rect()
+        center = (x + self.width // 2 - text_rect.width // 2, y + self.height // 2 - text_rect.height // 2)
+
+        if x <= mouse_pos[0] <= x + self.width and y <= mouse_pos[1] <= y + self.height:
+            if mouse_click[0]:
+                if self.sound:
+                    self.sound.play()
+                draw.rect(screen, self.pressed_bg, (x, y, self.width, self.height))
+                self.text.draw(screen, center)
+                self.action()
+
+                time.delay(100)
+            else:
+                draw.rect(screen, self.active_bg, (x, y, self.width, self.height))
+                self.text.draw(screen, center)
+        else:
+            draw.rect(screen, self.inactive_bg, (x, y, self.width, self.height))
+            self.text.draw(screen, center)
--- pgex/widgets/text.py
+++ pgex/widgets/text.py
@@ -10,6 +10,11 @@ class Text:
         self.bg_color = bg_color
         self.border_width = border_width
 
+    def get_rect(self):
+        font_type = font.Font(self.font_name, self.font_size)
+        text = font_type.render(self.text, True, self.font_color)
+        return text.get_rect()
+
     def draw(self, screen, coordinates):
         font_type = font.Font(self.font_name, self.font_size)
         text = font_type.render(self.text, True, self.font_color)

f2d69331dfe6a2a3c046fb11f22713ea2b6d8803
IvanFoke <58694429+IvanFoke@users.noreply.github.com>
2020-10-08 17:10:55 +0300

    Version changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pgex",
-    version="0.4.1",
+    version="0.4.3",
     author="Ivan Foke",
     author_email="ivan.foke@gmail.com",
     description="A Python PyGame package extension",
